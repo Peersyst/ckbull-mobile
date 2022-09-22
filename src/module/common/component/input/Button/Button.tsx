28314a3a6e78e0e04be1a39bc383c48231732fd2
JordiParraCrespo <jordiparra99@gmail.com>
2022-09-22 13:49:17 +0200

    fix

--- src/module/common/component/input/Button/Button.tsx
+++ src/module/common/component/input/Button/Button.tsx
@@ -2,11 +2,7 @@ import { ButtonProps } from "./Button.types";
 import { ButtonRoot } from "./Button.styles";
 
 const Button = ({ appearance = "dark", size = "lg", ...rest }: ButtonProps): JSX.Element => {
-    const a = {
-        ...rest,
-        a: "",
-    };
-    return <ButtonRoot size={size} appearance={appearance} {...a} />;
+    return <ButtonRoot size={size} appearance={appearance} {...rest} />;
 };
 
 export default Button;
