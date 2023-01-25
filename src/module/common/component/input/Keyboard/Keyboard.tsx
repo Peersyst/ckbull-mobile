7b05ed0ad39df81df9de0c81e91f75e04ede7043
JordiParraCrespo <jordiparra99@gmail.com>
2023-01-25 17:08:54 +0100

    improve keyboard

--- src/module/common/component/input/Keyboard/Keyboard.tsx
+++ src/module/common/component/input/Keyboard/Keyboard.tsx
@@ -23,24 +23,25 @@ const Keyboard = ({ setValue }: KeyboardProps): JSX.Element => {
         }
     };
 
+    const rowGap = "15%";
     return (
         <KeyboardRoot>
-            <Row gap="15%">
+            <Row gap={rowGap}>
                 <PadItem item={"1"} onPress={() => handlePress("1")} />
                 <PadItem item={"2"} onPress={() => handlePress("2")} />
                 <PadItem item={"3"} onPress={() => handlePress("3")} />
             </Row>
-            <Row gap="15%">
+            <Row gap={rowGap}>
                 <PadItem item={"4"} onPress={() => handlePress("4")} />
                 <PadItem item={"5"} onPress={() => handlePress("5")} />
                 <PadItem item={"6"} onPress={() => handlePress("6")} />
             </Row>
-            <Row gap="15%">
+            <Row gap={rowGap}>
                 <PadItem item={"7"} onPress={() => handlePress("7")} />
                 <PadItem item={"8"} onPress={() => handlePress("8")} />
                 <PadItem item={"9"} onPress={() => handlePress("9")} />
             </Row>
-            <Row gap="15%">
+            <Row gap={rowGap}>
                 <PadItem item={"X"} onPress={() => handlePress("X")} />
                 <PadItem item={"0"} onPress={() => handlePress("0")} />
                 <PadItem item={"<"} onPress={() => handlePress("<")} />
