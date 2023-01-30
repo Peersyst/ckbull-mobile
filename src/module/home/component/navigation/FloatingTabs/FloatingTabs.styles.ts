d5eda3d2b11b3ef89397bbff21e8eb284e5865e2
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-01-30 11:13:25 +0100

    FLoatingTabs ui fix

--- src/module/home/component/navigation/FloatingTabs/FloatingTabs.styles.ts
+++ src/module/home/component/navigation/FloatingTabs/FloatingTabs.styles.ts
@@ -1,16 +1,15 @@
 import styled from "@peersyst/react-native-styled";
 import { Col, TabGroup as BaseTabGroup } from "@peersyst/react-native-components";
 
-export const FloatingTabsGroup = styled(BaseTabGroup)(({ theme }) => ({
+export const FloatingTabsGroup = styled(BaseTabGroup)(() => ({
     gap: 16,
-    justifyContent: "center",
     paddingHorizontal: 10,
+}));
+
+export const FloatingTabsNavigatorRoot = styled(Col)(({ theme }) => ({
+    alignSelf: "center",
+    marginVertical: 20,
     borderWidth: 1,
     borderRadius: 8,
     borderColor: theme.palette.overlay[100]["12%"],
-}));
-
-export const FloatingTabsNavigatorRoot = styled(Col)(() => ({
-    alignItems: "center",
-    marginVertical: 20,
 }));
