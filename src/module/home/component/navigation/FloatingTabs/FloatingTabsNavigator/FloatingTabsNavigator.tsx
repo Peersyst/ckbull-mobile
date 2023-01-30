2f02ce3dcbeec14baf4640debe21a63678a7c920
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-01-30 11:27:32 +0100

    Indicator component

--- src/module/home/component/navigation/FloatingTabs/FloatingTabsNavigator/FloatingTabsNavigator.tsx
+++ src/module/home/component/navigation/FloatingTabs/FloatingTabsNavigator/FloatingTabsNavigator.tsx
@@ -1,15 +1,15 @@
 import { FloatingTabsGroup, FloatingTabsNavigatorRoot } from "module/home/component/navigation/FloatingTabs/FloatingTabs.styles";
-import { LinearGradient } from "expo-linear-gradient";
 import { useTheme } from "@peersyst/react-native-components";
 import BaseTab from "module/common/component/navigation/BaseTabs/BaseTab/BaseTab";
 import { FloatingTabsProps } from "module/home/component/navigation/FloatingTabs/FloatingTabs.types";
+import { View } from "react-native";
 
 const FloatingTabsNavigator = ({ tabs }: FloatingTabsProps): JSX.Element => {
     const { palette } = useTheme();
 
     return (
         <FloatingTabsNavigatorRoot>
-            <FloatingTabsGroup renderIndicator indicator={<LinearGradient colors={palette.gradient.greenGreen} />}>
+            <FloatingTabsGroup renderIndicator indicator={<View style={{ backgroundColor: palette.green[200] }} />}>
                 {tabs.map(({ title }, index) => {
                     return (
                         <BaseTab key={index} index={index} alternative>
--- src/module/main/component/navigation/MainTabs/MainTabsNavigator/MainTabsNavigator.tsx
+++ src/module/main/component/navigation/MainTabs/MainTabsNavigator/MainTabsNavigator.tsx
@@ -1,14 +1,14 @@
-import { LinearGradient } from "expo-linear-gradient";
 import { TabGroup } from "module/main/component/navigation/MainTabs/MainTabs.styles";
 import { MainTabsProps } from "module/main/component/navigation/MainTabs/MainTabs.types";
 import { useTheme } from "@peersyst/react-native-components";
 import BaseTab from "module/common/component/navigation/BaseTabs/BaseTab/BaseTab";
+import { View } from "react-native";
 
 const MainTabsNavigator = ({ tabs }: MainTabsProps): JSX.Element => {
     const { palette } = useTheme();
 
     return (
-        <TabGroup renderIndicator={true} indicator={<LinearGradient colors={palette.gradient.greenGreen} />}>
+        <TabGroup renderIndicator={true} indicator={<View style={{ backgroundColor: palette.green[200] }} />}>
             {tabs.map(({ title }, index) => {
                 return (
                     <BaseTab key={index} index={index}>
