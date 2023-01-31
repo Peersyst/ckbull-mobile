a822513893dd3e018d666bf15897fd1010dbabdf
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-01-31 18:16:34 +0100

    merge main

--- src/asset/image/index.ts
+++ src/asset/image/index.ts
@@ -2,10 +2,10 @@
 export { default as empty_folder } from "./EmptyFolder.png";
 export { default as animation } from "./animation.webp";
 export { default as deposit } from "./deposit.png";
+export { default as image_background } from "./image_background.png";
 export { default as key } from "./key.png";
 export { default as lock } from "./lock.png";
 export { default as notes } from "./notes.png";
 export { default as placeholder_image } from "./placeholder_image.jpg";
 export { default as send } from "./send.png";
 export { default as wallet } from "./wallet.png";
-export { default as image_background } from "./image_background.png";
