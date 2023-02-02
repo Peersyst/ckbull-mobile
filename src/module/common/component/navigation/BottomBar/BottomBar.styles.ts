679b55705fa0c6a64992e25dbd153317d4768186
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-02-02 12:03:42 +0100

    Fix

--- src/module/common/component/navigation/BottomBar/BottomBar.styles.ts
+++ src/module/common/component/navigation/BottomBar/BottomBar.styles.ts
@@ -1,11 +1,11 @@
 import styled from "@peersyst/react-native-styled";
 import { Row } from "@peersyst/react-native-components";
 
-export const BottomBarRoot = styled(Row, { justifyContent: "space-around", alignItems: "center" })(({ theme, safeAreaInsets }) => ({
+export const BottomBarRoot = styled(Row, { justifyContent: "space-around" })(({ theme, safeAreaInsets }) => ({
     backgroundColor: theme.palette.component.bottomBar,
     borderTopWidth: theme.borderWidth,
     borderTopColor: theme.palette.gray[100],
     paddingHorizontal: 10,
-    paddingBottom: safeAreaInsets.bottom + 15,
-    paddingTop: 5,
+    paddingBottom: safeAreaInsets.bottom + 8,
+    paddingTop: 8,
 }));
