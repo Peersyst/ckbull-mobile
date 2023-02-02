2dbb6dda2e38659f4df82a53c1ec966b9f645bc5
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-02-02 10:51:23 +0100

    Fix IconButton paddings

--- src/module/common/component/input/IconButton/IconButton.styles.ts
+++ src/module/common/component/input/IconButton/IconButton.styles.ts
@@ -6,13 +6,19 @@ export const IconButtonRoot = styled(Button)(() => ({
     lg: {
         width: BUTTON_SIZES.lg,
         fontSize: 28,
+        paddingHorizontal: 0,
+        paddingVertical: 0,
     },
     md: {
         width: BUTTON_SIZES.md,
         fontSize: 24,
+        paddingHorizontal: 0,
+        paddingVertical: 0,
     },
     sm: {
         width: BUTTON_SIZES.sm,
         fontSize: 20,
+        paddingHorizontal: 0,
+        paddingVertical: 0,
     },
 }));
