9579ec97eb453066e02f6779eea11f3b4fa4493f
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-02-02 16:17:52 +0000

    fixes from merging with dev

--- src/module/activity/queries/useGetConnectedSites.ts
+++ src/module/activity/queries/useGetConnectedSites.ts
@@ -1,11 +1,10 @@
 import { ConnectedSiteType } from "module/activity/component/display/ConnectedSite/ConnectedSite.types";
-import { ActivityActionKind } from "module/activity/core/ActivityAction/ActivityAction.types";
 
 /*
  *  MOCKED CALL PENDING TO BACKEND
  * */
 export default function () {
-    const mockSite: ConnectedSiteType = { title: "Figma", status: "connected", action: ActivityActionKind.DISCONNECT };
+    const mockSite: ConnectedSiteType = { title: "Figma", status: "connected" };
     const mockData: readonly any[] | null | undefined = [mockSite, mockSite, mockSite];
 
     return mockData;
