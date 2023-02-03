13c8ad011c9310baaf69baa26076744a086b7c0f
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-02-03 08:11:46 +0100

    Wake Jenkins up

--- src/module/common/component/navigation/BottomBar/BottomBarItem/BottomBarItem.styles.ts
+++ src/module/common/component/navigation/BottomBar/BottomBarItem/BottomBarItem.styles.ts
@@ -1,7 +1,7 @@
 import styled from "@peersyst/react-native-styled";
 import { Pressable } from "react-native";
-import { Icon, Typography } from "@peersyst/react-native-components";
 import { LinkItemIconProps, LinkTextProps } from "../BottomBar.types";
+import { Icon, Typography } from "@peersyst/react-native-components";
 
 export const LinkItemText = styled(Typography, { textAlign: "center" })<LinkTextProps>(({ theme, isActive }) => ({
     color: isActive ? theme.palette.gray[theme.palette.mode === "light" ? 600 : 900] : theme.palette.overlay[700]["48%"],
