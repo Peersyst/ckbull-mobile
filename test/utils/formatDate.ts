e9a7eb8b7f8d604690a8bbc77974fb8e00f759d7
JordiParraCrespo <jordiparra99@gmail.com>
2023-02-03 09:48:38 +0100

    add undefined to formatDateTest

--- test/utils/formatDate.ts
+++ test/utils/formatDate.ts
@@ -1,6 +1,6 @@
 import { formatDate } from "module/common/hook/useFormatDate";
 
-export const formatDateTest = (date: Date | string | number) => {
+export const formatDateTest = (date: Date | string | number | undefined) => {
     return formatDate("en", date);
 };
 
