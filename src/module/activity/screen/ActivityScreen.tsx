6b4c5f66e7211c415eef07726c707dc715422fb1
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-02-06 17:01:49 +0100

    adding space for search input

--- src/module/activity/screen/ActivityScreen.tsx
+++ src/module/activity/screen/ActivityScreen.tsx
@@ -1,9 +1,11 @@
 import BaseMainScreen from "module/main/component/layout/BaseMainScreen/BaseMainScreen";
 import ActivityTabs from "module/activity/component/navigation/ActivityTabs/ActivityTabs";
+import { View } from "react-native";
 
 const ActivityScreen = (): JSX.Element => {
     return (
         <BaseMainScreen>
+            <View style={{ backgroundColor: "gray", height: 60, marginTop: 20, marginBottom: 40 }}></View>
             <ActivityTabs />
         </BaseMainScreen>
     );
