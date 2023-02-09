5ae9258cdd6cd486679d0fc40885346eac76edfb
pablo <silvap.javier@gmail.com>
2023-02-09 21:57:12 +0100

    Fixs

--- src/module/wallet/component/display/AddWalletCard/AddWalletCard.style.ts
+++ src/module/wallet/component/display/AddWalletCard/AddWalletCard.style.ts
@@ -1,27 +1,11 @@
 import styled from "@peersyst/react-native-styled";
-import { PlusIcon } from "icons";
-import { Col, Typography } from "@peersyst/react-native-components";
+import { Col } from "@peersyst/react-native-components";
 
-export const AddIcon = styled(PlusIcon)(({ theme }) => ({
-    color: theme.palette.white,
-    fontSize: 64,
-}));
-
-export const AddText = styled(Typography, { textTransform: "uppercase" })(({ theme }) => ({
-    color: theme.palette.white,
-    fontWeight: "bold",
-}));
-
-export const AddWalletCardRoot = styled(Col)(({ theme }) => ({
+export const AddWalletCardRoot = styled(Col)(({ theme, dimensions }) => ({
     width: "100%",
     height: "100%",
-    padding: 24,
+    padding: Math.min(dimensions.height * 0.025, 24),
     overflow: "hidden",
     borderRadius: 20,
     backgroundColor: theme.palette.secondary,
-}));
-
-export const ContentRoot = styled(Col, { justifyContent: "center", gap: 20 })(() => ({
-    height: "100%",
-    backgroundColor: "red",
 }));
