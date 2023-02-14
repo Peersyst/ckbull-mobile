fbc74610970b9c961b01acbf8aefc25fc55f8c74
pablo <silvap.javier@gmail.com>
2023-02-15 00:32:43 +0100

    NftList

--- src/module/nft/component/core/NftsList/NftsList.tsx
+++ src/module/nft/component/core/NftsList/NftsList.tsx
@@ -3,11 +3,34 @@ import NftCard from "module/nft/component/display/NftCard/NftCard";
 import MainList from "module/main/component/display/MainList/MainList";
 import EmptyListComponent from "module/common/component/display/EmptyListComponent/EmptyListComponent";
 import useSelectedWallet from "module/wallet/hook/useSelectedWallet";
+import { Nft } from "ckb-peersyst-sdk";
+
+export const tempNft: Nft = {
+    tokenId: "0",
+    tokenUri: "https://ipfs.fleek.co/ipfs/bafybeiffkdczuvd6neggcggg63xd2ptdartq2rkzufob55qfkrrrby3kky",
+    nftName: "NEAR CAMEL 21 #19",
+    total: 10,
+    data: {
+        title: "NEAR CAMEL 21 #19",
+        description: "INDIAN CAMEL",
+        media: "https://ipfs.fleek.co/ipfs/bafybeiffkdczuvd6neggcggg63xd2ptdartq2rkzufob55qfkrrrby3kky",
+        media_hash: null,
+        copies: 1,
+        issued_at: null,
+        expires_at: null,
+        starts_at: null,
+        updated_at: null,
+        extra: null,
+        reference: null,
+        reference_hash: null,
+    },
+};
 
 const NftsList = (): JSX.Element => {
     const { index } = useSelectedWallet();
-    const { data = [], isLoading } = useGetNfts(index);
-
+    //const { data = [], isLoading } = useGetNfts(index);
+    const data = [tempNft];
+    const isLoading = false;
     return (
         <MainList
             loading={isLoading}
--- src/module/nft/component/display/NftCard/NftCard.tsx
+++ src/module/nft/component/display/NftCard/NftCard.tsx
@@ -1,4 +1,4 @@
-import { Col, Typography } from "@peersyst/react-native-components";
+import { Col, Row, Typography } from "@peersyst/react-native-components";
 import MainListCard from "module/main/component/display/MainListCard/MainListCard";
 import { TouchableWithoutFeedback } from "react-native";
 import { NftCardImage } from "./NftCard.styles";
@@ -15,26 +15,24 @@ const NftCard = ({ nft }: NftCardProps): JSX.Element => {
 
     return (
         <TouchableWithoutFeedback>
-            <MainListCard gap="6.5%">
-                <NftCardImage source={{ uri: tokenUri }} />
-                <Col flex={1} gap={12} justifyContent="center">
-                    <Col gap={2} flex={1}>
-                        {nftName && (
-                            <Typography variant="body1Strong" numberOfLines={1}>
-                                {nftName}
-                            </Typography>
-                        )}
-                        {description && (
-                            <Typography variant="body1Strong" numberOfLines={1}>
-                                {description}
-                            </Typography>
-                        )}
-                        {tokenId && total && (
-                            <Typography variant="body3Strong" numberOfLines={1} color="primary">
-                                {`${tokenId}/${total}`}
-                            </Typography>
-                        )}
-                    </Col>
+            <MainListCard gap={24} style={{ marginTop: 30, height: 100 }}>
+                <NftCardImage source={{ uri: tokenUri }} resizeMode="cover" />
+                <Col gap={2} flex={1}>
+                    {nftName && (
+                        <Typography variant="body2Strong" numberOfLines={1}>
+                            {nftName}
+                        </Typography>
+                    )}
+                    {description && (
+                        <Typography variant="body1Strong" numberOfLines={1}>
+                            {description}
+                        </Typography>
+                    )}
+                    {tokenId && total && (
+                        <Typography variant="body3Strong" numberOfLines={1} color="primary">
+                            {`${tokenId}/${total}`}
+                        </Typography>
+                    )}
                 </Col>
             </MainListCard>
         </TouchableWithoutFeedback>
