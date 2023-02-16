93c1d8606f9c21153a25ebbced22e4da1ef8bbf0
JordiParraCrespo <jordiparra99@gmail.com>
2023-02-16 16:09:06 +0100

    style fix linting

--- src/locale/i18n.types.d.ts
+++ src/locale/i18n.types.d.ts
@@ -2,8 +2,8 @@ import "i18next";
 import { defaultNS, resources } from "./i18n";
 
 export type LocaleType = "es" | "en" | "zh";
-export type NameSpacesType = "translation" | "error";
 export type ResourceType = typeof resources["en"];
+export type NameSpacesType = "translation" | "error";
 export type ErrorResourceType = keyof ResourceType["error"];
 export type TransaltionResourceType = keyof ResourceType["translation"];
 
