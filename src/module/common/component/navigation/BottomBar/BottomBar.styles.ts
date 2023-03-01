345e2cb28c646d92cf01f580ab02b487f14e0501
JordiParraCrespo <jordiparra99@gmail.com>
2023-03-01 17:12:21 +0100

    remove activity from the bottom bar

--- src/module/common/component/navigation/BottomBar/BottomBar.styles.ts
+++ src/module/common/component/navigation/BottomBar/BottomBar.styles.ts
@@ -1,11 +1,14 @@
 import styled from "@peersyst/react-native-styled";
 import { Row } from "@peersyst/react-native-components";
+import { config } from "config";
+
+export const VERTICAL_PADDING = config.enableSignerApp ? 12 : 8;
 
 export const BottomBarRoot = styled(Row, { justifyContent: "space-around" })(({ theme, safeAreaInsets }) => ({
     backgroundColor: theme.palette.component.bottomBar.backgroundColor,
     borderTopWidth: theme.borderWidth,
     borderTopColor: theme.palette.component.bottomBar.borderColor,
     paddingHorizontal: 10,
-    paddingBottom: safeAreaInsets.bottom + 8,
-    paddingTop: 8,
+    paddingBottom: safeAreaInsets.bottom + VERTICAL_PADDING,
+    paddingTop: VERTICAL_PADDING,
 }));
