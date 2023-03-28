3567f5a03a7ad173b08d157d07d7e724cce4fa54
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-03-28 10:37:44 +0200

    Components testing

--- test/unit/src/module/activity/component/display/SignInRequestDetails/SignInRequestDetails.spec.tsx
+++ test/unit/src/module/activity/component/display/SignInRequestDetails/SignInRequestDetails.spec.tsx
@@ -2,20 +2,39 @@ import { render, translate } from "test-utils";
 import SignRequestAppSummary from "module/activity/component/display/SignRequestAppSummary/SignRequestAppSummary";
 import { screen } from "@testing-library/react-native";
 import { UseWalletStateMock } from "mocks/common";
-import { wallet } from "images";
 
 describe("SignInRequestDetails tests", () => {
     const mockName = "name";
     const mockImage = "image";
     const mockDescription = "description";
+    const mockRequestTitle = "requestTitle";
 
     beforeEach(() => jest.restoreAllMocks());
 
-    test("Renders correctly without loading", () => {
+    test("Renders correctly without a selectedWallet", () => {
+        render(<SignRequestAppSummary requestTitle={mockRequestTitle} name={mockName} image={mockImage} description={mockDescription} />);
+
+        expect(screen.getByText(mockRequestTitle)).toBeDefined();
+        expect(screen.getByText(mockName)).toBeDefined();
+        expect(screen.getByText(mockDescription)).toBeDefined();
+        expect(screen.queryByText(translate("signWith"))).toBeNull();
+    });
+
+    test("Renders correctly if there is a selectedWallet", () => {
         const walletState = new UseWalletStateMock();
 
-        render(<SignRequestAppSummary name={mockName} image={mockImage} description={mockDescription} />);
+        render(
+            <SignRequestAppSummary
+                requestTitle={mockRequestTitle}
+                name={mockName}
+                image={mockImage}
+                description={mockDescription}
+                selectedWallet={1}
+                onWalletChange={jest.fn}
+            />,
+        );
 
+        expect(screen.getByText(mockRequestTitle)).toBeDefined();
         expect(screen.getByText(mockName)).toBeDefined();
         expect(screen.getByText(mockDescription)).toBeDefined();
         expect(screen.getByText(translate("signWith"))).not.toBeDisabled();
---
+++ test/unit/src/module/activity/component/display/SignTransactionRequestSuccess/SignTransactionRequestSuccess.spec.tsx
@@ -0,0 +1,51 @@
+import { capitalize } from "@peersyst/react-utils";
+import SignTransactionRequestSuccess from "module/activity/component/display/SignTransactionRequestSuccess/SignTransactionRequestSuccess";
+import { Linking } from "react-native";
+import { fireEvent, render, screen, translate } from "test-utils";
+import { UseServiceInstanceMock } from "../../../../../../../__mocks__/common/wallet/useServiceInstance.mock";
+
+describe("SignTransactionRequestSuccess tests", () => {
+    const mockTransactionHash = "transactionHash";
+
+    beforeEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    test("Renders correctly", () => {
+        new UseServiceInstanceMock();
+        render(<SignTransactionRequestSuccess transactionHash={mockTransactionHash} />);
+
+        expect(screen.getByText(capitalize(translate("transaction")))).toBeDefined();
+        expect(screen.getByText(mockTransactionHash)).toBeDefined();
+        expect(screen.getByText(capitalize(translate("copy")))).toBeDefined();
+        expect(screen.getByText(translate("seeInExplorer"))).toBeDefined();
+    });
+
+    test("Calls openURL with mainnet explorer", () => {
+        new UseServiceInstanceMock({ network: "mainnet" });
+        const openUrl = jest.spyOn(Linking, "openURL");
+
+        render(<SignTransactionRequestSuccess transactionHash={mockTransactionHash} />);
+
+        const explorerButton = screen.getByText(translate("seeInExplorer"));
+        expect(explorerButton).toBeDefined();
+
+        fireEvent.press(explorerButton);
+
+        expect(openUrl).toHaveBeenCalledWith(`https://explorer.nervos.org/transaction/${mockTransactionHash}`);
+    });
+
+    test("Calls openURL with testnet explorer", () => {
+        new UseServiceInstanceMock({ network: "testnet" });
+        const openUrl = jest.spyOn(Linking, "openURL");
+
+        render(<SignTransactionRequestSuccess transactionHash={mockTransactionHash} />);
+
+        const explorerButton = screen.getByText(translate("seeInExplorer"));
+        expect(explorerButton).toBeDefined();
+
+        fireEvent.press(explorerButton);
+
+        expect(openUrl).toHaveBeenCalledWith(`https://pudge.explorer.nervos.org/transaction/${mockTransactionHash}`);
+    });
+});
