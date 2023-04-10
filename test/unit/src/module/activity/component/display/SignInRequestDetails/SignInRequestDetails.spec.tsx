c26cb0deddeaee033493a504d6b107f906393465
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-04-10 12:03:22 +0200

    adding missing dtos

--- test/unit/src/module/activity/component/display/SignInRequestDetails/SignInRequestDetails.spec.tsx
+++ test/unit/src/module/activity/component/display/SignInRequestDetails/SignInRequestDetails.spec.tsx
@@ -2,21 +2,19 @@ import { render, translate } from "test-utils";
 import SignInRequestDetails from "module/activity/component/display/SignInRequestDetails/SignInRequestDetails";
 import { screen } from "@testing-library/react-native";
 import { UseWalletStateMock } from "mocks/common";
+import { SignInRequestDtoMock } from "mocks/common/activity/sign-in-request-dto.mock";
 
 describe("SignInRequestDetails tests", () => {
-    const mockName = "name";
-    const mockImage = "image";
-    const mockDescription = "description";
-
     beforeEach(() => jest.restoreAllMocks());
 
     test("Renders correctly without loading", () => {
         const walletState = new UseWalletStateMock();
+        const signInRequestMock = new SignInRequestDtoMock();
 
-        render(<SignInRequestDetails name={mockName} image={mockImage} description={mockDescription} />);
+        render(<SignInRequestDetails signInRequest={signInRequestMock} />);
 
-        expect(screen.getByText(mockName)).toBeDefined();
-        expect(screen.getByText(mockDescription)).toBeDefined();
+        expect(screen.getByText(signInRequestMock.app.name)).toBeDefined();
+        expect(screen.getByText(signInRequestMock.app.description)).toBeDefined();
         expect(screen.getByText(translate("signWith"))).not.toBeDisabled();
         expect(walletState.mock).toHaveBeenCalled();
     });
