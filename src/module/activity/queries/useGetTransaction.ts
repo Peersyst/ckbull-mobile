5438043a837f5e27809732da7d952b408e380b75
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-04-11 11:12:00 +0200

    hotfix in useGetTransaction

--- src/module/activity/queries/useGetTransaction.ts
+++ src/module/activity/queries/useGetTransaction.ts
@@ -1,18 +1,19 @@
 import Queries from "../../../query/queries";
 import { useQuery } from "react-query";
 
-export default function useGetTransaction(id: number) {
-    const mockCKBSdkTransaction = () => {
-        return {
-            senders: [
-                "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqfafsuem9n6en7pd8z4he0clxpy8fsx4scfheym5",
-                "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqge0t60zaexkn7yhcsel9rvmg3xqp3dn3qgw3z7k",
-            ],
-            receivers: ["ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqge0t60zaexkn7yhcsel9rvmg3xqp3dn3qgw3z7k"],
-            amount: 1000000,
-            type: "ckb",
-        };
+const mockCKBSdkTransaction = (id: number) => {
+    return {
+        senders: [
+            "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqfafsuem9n6en7pd8z4he0clxpy8fsx4scfheym5",
+            "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqge0t60zaexkn7yhcsel9rvmg3xqp3dn3qgw3z7k",
+        ],
+        receivers: ["ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqge0t60zaexkn7yhcsel9rvmg3xqp3dn3qgw3z7k"],
+        amount: 1000000,
+        type: "ckb",
+        id,
     };
+};
 
-    return useQuery([Queries.GET_TRANSACTION], () => mockCKBSdkTransaction());
+export default function useGetTransaction(id: number) {
+    return useQuery([Queries.GET_TRANSACTION], () => mockCKBSdkTransaction(id));
 }
