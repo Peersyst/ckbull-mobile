a1bb768ebdadfbcd410f772561c843c1440c2765
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-04-19 17:05:41 +0200

    deleted unused hooks

--- src/module/activity/queries/useGetTransactionRequest.ts
+++
@@ -1,9 +0,0 @@
-import { TransactionRequestService } from "module/api/service";
-import Queries from "query/queries";
-import { useQuery } from "react-query";
-
-export default function useGetTransactionRequest(transactionRequestToken: string) {
-    return useQuery(Queries.GET_TRANSACTION_REQUEST, () =>
-        TransactionRequestService.getTransactionRequestByTransactionToken(transactionRequestToken),
-    );
-}
