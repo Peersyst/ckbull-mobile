e0a132f060f11ebe61f2a20de1e95fe4d240f496
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-05-11 15:06:31 +0200

    fix(SignerFixes): SonarCloud bug fix

--- test/unit/src/module/home/core/AccountSlider.spec.tsx
+++ test/unit/src/module/home/core/AccountSlider.spec.tsx
@@ -3,9 +3,18 @@ import { render } from "test-utils";
 import { UseServiceInstanceMock, UseWalletStateMock } from "test-mocks";
 
 describe("Test for the Home Slider", () => {
-    const { state } = new UseWalletStateMock();
-    new UseServiceInstanceMock();
+    let serviceInstance: UseServiceInstanceMock;
+
+    beforeEach(() => {
+        serviceInstance = new UseServiceInstanceMock();
+    });
+
+    afterEach(() => {
+        serviceInstance.restore();
+    });
+
     test("Renders correctly with cells", () => {
+        const { state } = new UseWalletStateMock();
         const screen = render(<AccountSlider />);
         expect(screen.getAllByText(state.wallets[0].name)).toBeDefined();
         expect(screen.getAllByText(state.wallets[1].name)).toBeDefined();
