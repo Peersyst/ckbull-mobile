8a24a6e957e4cfc7849698407afab85ad5c231ee
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-03-01 09:58:25 +0100

    fix: remove intl

--- App.tsx
+++ App.tsx
@@ -1,16 +1,3 @@
-import Root from "./src";
-import useCachedResources from "./src/module/common/hook/useCachedResources";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from "react";
-//Explanation link :https://github.com/facebook/react-native/issues/12981#issuecomment-652745831
-import { LogBox } from "react-native";
-
-const App = (): JSX.Element | null => {
-    const isLoadingComplete = useCachedResources();
-    LogBox.ignoreLogs(["Setting a timer"]);
-    LogBox.ignoreLogs(["Require cycles"]);
-    LogBox.ignoreLogs(["Require cycle:"]);
-    return isLoadingComplete ? <Root /> : null;
-};
+import App from "./src";
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import "./polyfills";
+
 import Providers from "./Providers";
 import Navigator from "./navigator/Navigator";
 import { useLoad } from "module/common/query/useLoad";
@@ -5,10 +7,9 @@ import LogoPage from "module/common/component/layout/LogoPage/LogoPage";
 import { StatusBar, Suspense } from "@peersyst/react-native-components";
 import { useRecoilValue } from "recoil";
 import settingsState from "module/settings/state/SettingsState";
-import { Platform, UIManager } from "react-native";
+import { Platform, UIManager, LogBox } from "react-native";
+import useCachedResources from "./module/common/hook/useCachedResources";
 import "module/api/OpenApiConfig";
-
-if (typeof BigInt === "undefined") global.BigInt = require("big-integer");
 
 if (Platform.OS === "android") {
     if (UIManager.setLayoutAnimationEnabledExperimental) {
@@ -16,13 +17,18 @@ if (Platform.OS === "android") {
     }
 }
 
+LogBox.ignoreLogs(["Setting a timer"]);
+LogBox.ignoreLogs(["Require cycles"]);
+LogBox.ignoreLogs(["Require cycle:"]);
+
 const App = (): JSX.Element => {
     const loading = useLoad();
+    const cachedResourcesLoaded = useCachedResources();
     const { loading: loadingSettings = false } = useRecoilValue(settingsState);
 
     return (
         <>
-            <Suspense fallback={<LogoPage />} isLoading={loading || loadingSettings}>
+            <Suspense fallback={<LogoPage />} isLoading={loading || loadingSettings || !cachedResourcesLoaded}>
                 <Navigator />
             </Suspense>
             <StatusBar appearance="dark" />
